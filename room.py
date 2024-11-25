7c409826f0a04d50589839f21dc0bac5ea390357
khadija-wahhabi <khadija.wahhabi@edu.esiee.fr>
2024-11-25 13:20:52 +0100

    Update room.py

--- room.py
+++ room.py
@@ -5,8 +5,28 @@ Authors = WAHHABI Khadija & BOUASSRIA Lamyae
 class Room:
     """
      Classe pour l'implémentation des rooms
-
     """
+
+    # Dictionnaire des zones et des chemins
+    zones = {
+        "entrée": {"nord": "salon", "est": None, "sud": None, "ouest": None},
+        "salon": {"nord": "chambre", "est": "cuisine", "sud": "entrée", "ouest": None},
+        "cuisine": {"nord": "jardin", "est": None, "sud": None, "ouest": "salon"},
+        "chambre": {"nord": None, "est": None, "sud": "salon", "ouest": "grenier"},
+        "grenier": {"nord": None, "est": "chambre", "sud": None, "ouest": None},
+        "jardin": {"nord": None, "est": None, "sud": "cuisine", "ouest": None}
+    }
+
+    # Zones interdites
+    zones_interdites = ["chambre", "grenier", "jardin"]  
+
+    # Sens unique
+    sens_unique = {
+        "entrée": {"nord": True},  # Peut aller vers le salon uniquement
+        "salon": {"nord": True, "sud": True},  # Ne peut pas revenir directement à l'entrée
+        "cuisine": {"sud": False},  # Ne peut pas revenir au jardin
+        "chambre": {"ouest": False}  # Ne peut pas aller au grenier depuis la chambre
+    }
 
     # Define the constructor.
     def __init__(self, name, description):
@@ -18,78 +38,4 @@ class Room:
         self.pnj = {}
         self.cle = False
 
-    # Define the get_exit method.
-    def get_exit(self, direction):
-        """
-        Permet d'obtenir les sorties.
 
-        """
-        # Return the room in the given direction if it exists.
-        if direction in self.exits:
-            return self.exits[direction]
-        return None
-
-    # Return a string describing the room's exits.
-    def get_exit_string(self):
-        """
-        Affiche les sorties.
-
-        """
-        exit_string = "Sorties: "
-        for ex in self.exits:
-            if self.exits.get(ex) is not None:
-                exit_string += ex + ", "
-        exit_string = exit_string.strip(", ")
-        return exit_string
-
-    # Return a long description of this room including exits.
-    def get_long_description(self):
-        """
-        Retourne une longue description de la room.
-
-        """
-        self.get_indices()
-        return f"\nVous êtes {self.description}\n\n{self.get_exit_string()}\n"
-
-    def get_name(self):
-        """
-        Retourne le nom de la room.
-
-        """
-        return self.name
-
-    def get_inventory(self):
-        """
-        Affiche l'inventaire de la pièce.
-
-        """
-        if len(self.inventory)==0 and len(self.pnj)==0:
-            print("Il n'y a rien ici.")
-        else:
-            print("On voit :")
-            for i in self.inventory.values():
-                print("\t- ",end="")
-                print(i)
-            for i in self.pnj.values():
-                print("\t- ",end="")
-                print(i)
-
-
-    def get_indices(self):
-        """
-        Préviens le joueur qu'un jeu un disponible dans la room.
-
-        """
-        if self.indice is True:
-            print("\nUn jeu est disponible, entrez \"jouer\" pour tenter votre chance")
-
-    def get_exlist(self):
-        """
-        Affiche un tableau contenant les directions vers lesquelles une pièce est présente.
-
-        """
-        exlist = []
-        for ex in self.exits:
-            if self.exits.get(ex) is not None:
-                exlist.append(ex)
-        return exlist
