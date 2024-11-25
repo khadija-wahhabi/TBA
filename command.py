9badc60a0e882614262411f3b129d69f6c05fa4c
khadija-wahhabi <khadija.wahhabi@edu.esiee.fr>
2024-11-25 13:23:32 +0100

    Update command.py

--- command.py
+++ command.py
@@ -4,35 +4,6 @@ Authors = WAHHABI Khadija & BOUASSRIA Lamyae
 """
 
 class Command:
-    """
-    This class represents a command. 
-    A command is composed of a command word,
-      a help string, an action and a number of parameters.
-
-    Attributes:
-        command_word (str): The command word.
-        help_string (str): The help string.
-        action (function): The action to execute when the command is called.
-        number_of_parameters (int): The number of parameters expected by the command.
-
-    Methods:
-        __init__(self, command_word, help_string, action, number_of_parameters) : The constructor.
-        __str__(self) : The string representation of the command.
-
-    Examples:
-
-    >>> from actions import go
-    >>> command = Command("go", "Permet de se déplacer dans une direction.", go, 1)
-    >>> command.command_word
-    'go'
-    >>> command.help_string
-    'Permet de se déplacer dans une direction.'
-    >>> type(command.action)
-    <class 'function'>
-    >>> command.number_of_parameters
-    1
-
-    """
 
     # The constructor.
     def __init__(self, command_word, help_string, action, number_of_parameters):
