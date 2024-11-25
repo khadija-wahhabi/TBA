4ed9a420bad1ee82e80b65041a0d9aecf8b9c227
khadija-wahhabi <khadija.wahhabi@edu.esiee.fr>
2024-11-25 13:11:57 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,28 +1,3 @@
 """
 Authors = WAHHABI Khadija & BOUASSRIA Lamyae
 """
-
-import tkinter as tk
-from gameinterface import GameInterface
-from game import Game
-
-def launch_game():
-    """
-    Fonction pour relancer le jeu
-    """
-    root = tk.Tk()
-    game_instance = Game()
-    my_game_interface = GameInterface(root, game_instance)
-    root.mainloop()
-    return my_game_interface.should_restart
-
-def main():
-    """
-    Fonction Main
-    """
-    should_restart = True
-    while should_restart:
-        should_restart = launch_game()
-
-if __name__ == "__main__":
-    main()
