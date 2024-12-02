d9eb1e4584c78c4524bc43cf7ddd9d443afe826c
khadija-wahhabi <khadija.wahhabi@edu.esiee.fr>
2024-12-02 11:16:01 +0100

    Update player.py

--- player.py
+++ player.py
@@ -1,1 +1,27 @@
+"""
+Fichier : player.py
+Contient la classe Player pour gérer le joueur.
+Authors = WAHHABI Khadija & BOUASSRIA Lamyae
+"""
+
+class Player:
+    def __init__(self, start_position):
+        # Initialisation du joueur.
+        self.position = start_position  # Position actuelle
+        self.inventory = []  # Inventaire du joueur
+
+    def changer_position(self, nouvelle_position):
+        # Change la position actuelle du joueur.
+        self.position = nouvelle_position
+
+    def ajouter_objet(self, objet):
+        # Ajoute un objet à l'inventaire du joueur.
+        self.inventory.append(objet)
+
+    def afficher_inventaire(self):
+        # Affiche le contenu de l'inventaire du joueur.
+        if self.inventory:
+            print("Votre inventaire : " + ", ".join(self.inventory))
+        else:
+            print("Votre inventaire est vide.")
 
