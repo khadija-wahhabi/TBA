e4e21d3f6fd898e7b50e39842cdc8eaca44ece6a
khadija-wahhabi <khadija.wahhabi@edu.esiee.fr>
2024-12-02 10:53:26 +0100

    Update command.py

--- command.py
+++ command.py
@@ -1,11 +1,10 @@
-# This file contains the Command class.
 """
 Authors = WAHHABI Khadija & BOUASSRIA Lamyae
 """
 
 class Command:
 
-    # The constructor.
+    # Le constructeur
     def __init__(self, command_word, help_string, action, number_of_parameters):
         self.command_word = command_word
         self.help_string = help_string
@@ -14,8 +13,13 @@ class Command:
 
     # The string representation of the command.
     def __str__(self):
-        return  self.command_word \
-                + self.help_string
+        return f"{self.command_word} - {self.help_string}"
 
     def afficher_message_erreur():
         print("Commande non reconnue. Entrez 'help' pour voir la liste des commandes disponibles.")
+    def executer(self, *params):
+    if len(params) != self.number_of_parameters:
+        print("Nombre incorrect de paramètres pour cette commande.")
+    else:
+        return self.action(*params)
+
