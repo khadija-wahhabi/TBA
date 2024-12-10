0c6a47abdaf4298702c43736ba96b1c3c0931015
khadija-wahhabi <khadija.wahhabi@edu.esiee.fr>
2024-12-10 10:23:35 +0100

    Update main.py

--- main.py
+++ main.py
@@ -7,18 +7,8 @@ Authors = WAHHABI Khadija & BOUASSRIA Lamyae
 from game import Game
 
 def main():
-    print("=== Bienvenue dans notre jeu texte ! ===")
-    print("Vous vous réveillez dans la maison de vos grands-parents, sans souvenirs de votre passé.")
-    print("Votre mission : explorer la maison, résoudre des énigmes, et retrouver votre mémoire.")
-    print("Tapez 'help' à tout moment pour afficher les commandes disponibles.")
-    
-    # Créer une instance de Game et démarrer le jeu
     game = Game()
     game.jouer()
-
-if __name__ == "__main__":
-    main()
-
 
 # Démarrage du jeu
 if __name__ == "__main__":
